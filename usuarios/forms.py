2daeca06f1b6e4848a63bba87b658ef92928d3ed
berllocK <brenosouzaemp@gmail.com>
2025-02-23 22:43:09 -0300

    Refatoring code

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -71,3 +71,13 @@ class CadastroForms(forms.Form):
             }
         )
     )
+
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get("nome_cadastro")
+
+        if nome:
+            nome = nome.strip()
+            if " " in nome:
+                raise forms.ValidationError("Nao e possivel inserir espacos dentro do campo usuario")
+            else:
+                return nome
